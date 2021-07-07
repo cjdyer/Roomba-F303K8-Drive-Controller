553150f790a4d5b684d6858bbe95a10da3e30924
Craig Martin-Smith <craigms@oakhenge.co.nz>
2021-07-08 10:50:27 +1200

    Simplified Example Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
 #include <Arduino.h>
 
-/*
-  Timebase callback
-  This example shows how to configure HardwareTimer to execute a callback at regular interval.
-  Callback toggles pin.
-  Once configured, there is only CPU load for callbacks executions.
-*/
-
 #if !defined(STM32_CORE_VERSION) || (STM32_CORE_VERSION  < 0x01090000)
-#error "Due to API change, this sketch is compatible with STM32_CORE_VERSION  >= 0x01090000"
-#endif
-
-#if defined(LED_BUILTIN)
-#define pin  LED_BUILTIN
-#else
-#define pin  D2
+  #error "Due to API change, this sketch is compatible with STM32_CORE_VERSION  >= 0x01090000"
 #endif
 
 unsigned int counter1 = 0;
@@ -22,39 +9,22 @@ unsigned int counter2 = 0;
 unsigned int counter3 = 0;
 unsigned int counter4 = 0;
 
-#if defined(TIM1)
-    TIM_TypeDef *Instance1 = TIM1;
-#else
-    TIM_TypeDef *Instance = TIM2;
-#endif
-
-
+// Define Timer instances
+TIM_TypeDef *Instance1 = TIM1;
 TIM_TypeDef *Instance2 = TIM2;
 
+// Define Timer Objects
 HardwareTimer *Timer1 = new HardwareTimer(Instance1);
 HardwareTimer *Timer2 = new HardwareTimer(Instance2);
 
-void ledFlash_callback(void){ 
-    // Toggle pin. 10hz toogle --> 5Hz PWM
-    digitalWrite(pin, !digitalRead(pin));
-    counter1++;
-}
-
-void print_callback(void){
-    // Toggle pin. 10hz toogle --> 5Hz PWM
-    counter2++;
-}
-
-void btn1_callback(void){
-    counter3++;
-}
-
-void btn2_callback(void){
-    counter4++;
-}
+// Callback Functions
+void ledFlash_callback(void)  {counter1++; digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));} // Toggle pin. 10hz toogle --> 5Hz PWM
+void print_callback(void)     {counter2++;}
+void btn1_callback(void)      {counter3++;}
+void btn2_callback(void)      {counter4++;}
 
 void setup(){
-    
+
     Serial.begin(115200);
     // Instantiate HardwareTimer object. Thanks to 'new' instanciation, HardwareTimer is not destructed when setup() function is finished.
     //HardwareTimer *MyTim = new HardwareTimer(Instance);
@@ -65,7 +35,7 @@ void setup(){
     pinMode(5, INPUT_PULLDOWN);
     pinMode(6, INPUT_PULLDOWN);
     noInterrupts();
-    
+
         Timer1->setOverflow(2, HERTZ_FORMAT); // 10 Hz
         Timer1->attachInterrupt(ledFlash_callback);
 
