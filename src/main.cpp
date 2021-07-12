88cf7cfa0c58d2606e7307ab31060c2e887498c7
Craig Martin-Smith <craigms@oakhenge.co.nz>
2021-07-12 14:44:34 +1200

    Implemented functional techometry

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ int LNew = 0; // New encoder value left motor
 int RNew = 0; // New encoder value right motor
 
 unsigned int counter1 = 0;
-unsigned int iter = 0;
+int iter = 1;
 
 unsigned int start_time;
 unsigned int end_time;
@@ -116,6 +116,27 @@ bool speedCheck = 0;
 
 float rpm_coefficient = ((600 / 0.05) / fullRev);
 
+const uint16_t PulsesPerRevolution = 1633;
+const unsigned long ZeroTimeout = 100000; 
+const byte numReadings = 10;
+volatile unsigned long LastTimeWeMeasured;
+unsigned long CurrentMicros = micros();
+volatile unsigned long PeriodBetweenPulses = ZeroTimeout+1000;
+volatile unsigned long PeriodAverage = ZeroTimeout+1000;
+unsigned long FrequencyRaw;
+unsigned long FrequencyReal;
+unsigned long RPM;
+unsigned int PulseCounter = 1;
+unsigned long PeriodSum;
+unsigned long LastTimeCycleMeasure = LastTimeWeMeasured;
+unsigned int AmountOfReadings = 1;
+unsigned int ZeroDebouncingExtra;
+
+// Variables for smoothing tachometer:
+unsigned long readings[numReadings];  // The input.
+unsigned long readIndex;  // The index of the current reading.
+unsigned long total;  // The running total.
+unsigned long average;  // The RPM value after applying the smoothing.
 
 // Speed Calc Callback
 void speedCalc_callback(void){
@@ -174,19 +195,42 @@ void speedCalc_callback(void){
 
 // Left Hall encoder. Called once for each sensor on pin-change (quadrature)
 void encoderLeft_callback(void){
-  if(Lrun){
+  /*if(Lrun){
     LOld = LNew;
     // Access the Digital Input Register directly - MUCH faster than digitalRead()
     LNew = ((GPIOA->IDR >> 0) & 1) * 2 + ((GPIOA->IDR >> 1) & 1); // Convert binary input to decimal value // (PINC & 0b0001) gets the value of bit zero in port C (A0), ((PINC & 0b0010) >> 1) gets just the value of A1
     encoderPosL += QEM[LOld * 4 + LNew];
     wheelSpeedDistanceL += QEM[LOld * 4 + LNew];
-    Lfired = 1;
+    Lfired = 1; 
+  }*/
+  
+  PeriodBetweenPulses   = micros() - LastTimeWeMeasured;
+  LastTimeWeMeasured    = micros();
+  
+  if(PulseCounter >= AmountOfReadings)  // If counter for amount of readings reach the set limit:
+  {
+    PeriodAverage = PeriodSum / AmountOfReadings;
+    PulseCounter = 1;  // Reset the counter to start over. The reset value is 1 because its the minimum setting allowed (1 reading).
+    PeriodSum = PeriodBetweenPulses;  // Reset PeriodSum to start a new averaging operation.
+
+    int RemapedAmountOfReadings = map(PeriodBetweenPulses, 40000, 5000, 1, 10);  // Remap the period range to the reading range.
+    // 1st value is what are we going to remap. In this case is the PeriodBetweenPulses.
+    // 2nd value is the period value when we are going to have only 1 reading. The higher it is, the lower RPM has to be to reach 1 reading.
+    // 3rd value is the period value when we are going to have 10 readings. The higher it is, the lower RPM has to be to reach 10 readings.
+    // 4th and 5th values are the amount of readings range.
+    RemapedAmountOfReadings = constrain(RemapedAmountOfReadings, 1, 10);  // Constrain the value so it doesn't go below or above the limits.
+    AmountOfReadings = RemapedAmountOfReadings;  // Set amount of readings as the remaped value.
   }
-}
+  else
+  {
+    PulseCounter++;  // Increase the counter for amount of readings by 1.
+    PeriodSum = PeriodSum + PeriodBetweenPulses;  // Add the periods so later we can average.
+  }
+} // end encoderLeft_callback
 
 // Right Hall encoder. Called once for each sensor on pin-change (quadrature)
 void encoderRight_callback(void){
-  if(Rrun){
+  /*if(Rrun){
     ROld = RNew;
     // Access the Digital Input Register directly - MUCH faster than digitalRead()
     // encoderRA = 4th bit of GPIO port A, encoderRB = 5th bit
@@ -194,7 +238,7 @@ void encoderRight_callback(void){
     encoderPosR += QEM[ROld * 4 + RNew];
     wheelSpeedDistanceR += QEM[ROld * 4 + RNew];
     Rfired = 1;
-  }
+  }*/
 }
 
 void printFormattedFloat(float val, uint8_t leading, uint8_t decimals){
@@ -326,6 +370,8 @@ void printBin(uint16_t input){
   }
 }
 
+int iter_coeff = 1;
+
 void loop(){
   if(running == 1){
     /*for(int i=0; i <= 250; i++){
@@ -333,9 +379,9 @@ void loop(){
       delay(10);
     }*/
     ///*
-    //if(0){
-    iter++;
-    if(speedCheck){
+    if(0){
+    
+    //if(speedCheck){
       speedCheck = 0;
       //Lfired = 0;
       //Rfired = 0;
@@ -410,10 +456,15 @@ void loop(){
     //wheelSpeedL = 60 * (encoderPosL/fullRev)/0.01;
 
     pidLeft.Compute();
-    if(iter < 250){
+    if(iter > 0){
+      iter += iter_coeff;
       wheelSpeedL_desired = map(iter, 0, 250, 0, 20);
       motorL.drive(iter);
-      motorR.drive(iter);
+      //motorR.drive(iter);
+      if(iter == 255){
+        iter_coeff = -1;
+        //Serial.println("Switch dir");
+      }
       delay(20);
     
     }else{
@@ -422,6 +473,88 @@ void loop(){
       Lrun = 0;
       Rrun = 0;
     }
+
+// ###########################################
+// ###########################################
+// ###########################################
+
+///*
+// The following is going to store the two values that might change in the middle of the cycle.
+// We are going to do math and functions with those values and they can create glitches if they change in the
+// middle of the cycle.
+LastTimeCycleMeasure = LastTimeWeMeasured;  // Store the LastTimeWeMeasured in a variable.
+CurrentMicros = micros();  // Store the micros() in a variable.
+
+// CurrentMicros should always be higher than LastTimeWeMeasured, but in rare occasions that's not true.
+// I'm not sure why this happens, but my solution is to compare both and if CurrentMicros is lower than
+// LastTimeCycleMeasure I set it as the CurrentMicros.
+// The need of fixing this is that we later use this information to see if pulses stopped.
+if(CurrentMicros < LastTimeCycleMeasure)
+{
+  LastTimeCycleMeasure = CurrentMicros;
+}
+
+// Calculate the frequency:
+FrequencyRaw = 10000000000 / PeriodAverage;  // Calculate the frequency using the period between pulses.
+
+// Detect if pulses stopped or frequency is too low, so we can show 0 Frequency:
+if(PeriodBetweenPulses > ZeroTimeout - ZeroDebouncingExtra || CurrentMicros - LastTimeCycleMeasure > ZeroTimeout - ZeroDebouncingExtra)
+{  // If the pulses are too far apart that we reached the timeout for zero:
+  FrequencyRaw = 0;  // Set frequency as 0.
+  ZeroDebouncingExtra = 2000;  // Change the threshold a little so it doesn't bounce.
+}
+else
+{
+  ZeroDebouncingExtra = 0;  // Reset the threshold to the normal value so it doesn't bounce.
+}
+
+FrequencyReal = FrequencyRaw / 10000;  // Get frequency without decimals.
+                                    // This is not used to calculate RPM but we remove the decimals just in case
+                                    // you want to print it.
+
+// Calculate the RPM:
+RPM = FrequencyRaw / PulsesPerRevolution * 60;  // Frequency divided by amount of pulses per revolution multiply by
+                                            // 60 seconds to get minutes.
+RPM = RPM / 10000;  // Remove the decimals.
+
+// Smoothing RPM:
+total = total - readings[readIndex];  // Advance to the next position in the array.
+readings[readIndex] = RPM;  // Takes the value that we are going to smooth.
+total = total + readings[readIndex];  // Add the reading to the total.
+readIndex = readIndex + 1;  // Advance to the next position in the array.
+
+if (readIndex >= numReadings)  // If we're at the end of the array:
+{
+  readIndex = 0;  // Reset array index.
+}
+
+// Calculate the average:
+average = total / numReadings;  // The average value it's the smoothed result.
+
+// Print information on the serial monitor:
+// Comment this section if you have a display and you don't need to monitor the values on the serial monitor.
+// This is because disabling this section would make the loop run faster.
+//Serial.print("iter: ");
+Serial.print(iter);
+Serial.print("\t");
+//Serial.print("\tPeriod: ");
+//Serial.print(PeriodBetweenPulses);
+//Serial.print("\tReadings: ");
+//Serial.print(AmountOfReadings);
+//Serial.print("\tFrequency: ");
+//Serial.print(FrequencyReal);
+//Serial.print("\tRPM: ");
+Serial.print(RPM);
+Serial.print("\t");
+//Serial.print("\tTachometer: ");
+Serial.println(average);
+//Serial.println();
+
+//*/
+
+// ###########################################
+// ###########################################
+// ###########################################
 
     /*if(wheelSpeedDistanceL >= desiredDistance){
       if(Lrun){
